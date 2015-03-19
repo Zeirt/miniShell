d02ed3df6e0f914f76b354ef685b147461c7f6e2
BeatrizCortes <bea-cortes@hotmail.com>
2015-03-19 16:30:57 +0100

    execute.c basic function updated, now compiles

--- execute.c
+++ execute.c
@@ -13,12 +13,26 @@ void execute_external_command(const char *command)
 	char **args;
 	int backgr=0;
 	pid_t pid;
-	if ((args=parser_command(command,&backgr))==NULL)
+	if ((args=parser_command(command,&backgr))==NULL) 
 	{
-		return;
+		return 1; //code 1= there is no command
 	}
-
-	//code here
-
-	return;
+	
+	pid=fork(); //create a child process
+	
+	if(pid==0)
+	{
+		printf("I am child: %d\n",pid);
+	} else if (pid>0)
+		{
+			printf("I am parent: %d\n",pid);
+		} 
+		else
+		{
+			printf("Fork exploded");
+			_exit(2); //exit on failure
+		}
+	
+	parser_free_args(args);//free memory
+	return 0; //all ended well
 }
--- makefile
+++ makefile
@@ -1,16 +1,19 @@
 CFLAGS= -c -g -Wall
 OFLAGS= -L./lib/Linux/i686 -lshell -o
 
-all: free_args.o minishell_input.o minishell.o minishell
+all: execute.o  free_args.o minishell_input.o minishell.o minishell
 
 minishell: minishell.o 
-	gcc minishell.o  minishell_input.o free_args.o  $(OFLAGS) minishell
+	gcc minishell.o execute.o  minishell_input.o free_args.o  $(OFLAGS) minishell
 
 free_args.o: free_args.c
 	gcc free_args.c $(CFLAGS)
 
 minishell_input.o: minishell_input.c
 	gcc minishell_input.c $(CFLAGS)
+
+execute.o: execute.c
+	gcc execute.c $(CFLAGS)
 
 minishell.o: minishell.c 
 	gcc minishell.c $(CFLAGS)
