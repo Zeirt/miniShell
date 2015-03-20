b9e4d20c82600cbdc3aab0a0472772f34ccc2cce
Beatriz Cortés <beatriz.cortes@edu.uah.es>
2015-03-20 12:28:28 +0100

    Bg and fg processes work right now

--- execute.c
+++ execute.c
@@ -21,19 +21,6 @@ void execute_external_command(const char *command)
 	
 	pid=fork(); //create a child process
 	
-	/*if(pid==0)
-	{
-		printf("I am child: %d\n",pid);
-	} else if (pid>0)
-		{
-			printf("I am parent: %d\n",pid);
-		} 
-		else
-		{
-			printf("Fork exploded");
-			_exit(1); //exit on failure
-		}*/
-
 	if(pid==0)//will be 0 if it's the parent
 	{
 		ppid=getpid(); //get the pid of parent
@@ -46,7 +33,14 @@ void execute_external_command(const char *command)
 	else if (pid>0) //won't be 0 if it's the child
 		{
 			//jobs_new(pid,'c');//add name stuff later, registers child
-			ppid=wait(&status); //parent waits for child to finish
+			if(backgr==0) //no bg, if bg, parent doesn't wait
+			{
+				ppid=waitpid(pid,&status,WNOHANG); //parent waits for child to finish
+			}
+			else
+			{
+				ppid=wait(&status); //parent doesn't block
+			}
 		}
 	
 	jobs_finished(pid); //indicates the job/process is done
